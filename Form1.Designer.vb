84becfd2943776c08058bdadf9e393a837060ab1
eilli <eilli@LAPTOP-D1VN7TNU>
2023-11-23 17:45:11 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(118, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(154, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(154, 141)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(157, 95)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(265, 181)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(62, 178)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(197, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "CLICK FOR AGE IN DAYS -->"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(64, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "FULL NAME:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(64, 100)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "DOB:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(64, 141)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 16)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "AGE:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(118, 376)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "EXIT"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Security.Cryptography
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        '1)write down full name
+        'click button, inputBox1 will appear
+        'inputBox1 request for full name, OK /
+        'inputBox2 request for birth date, OK /
+        'getting today's date and calculate how old are you in DAYS
+        'display your age in DAYS (eg: 7300 days old)/
+        'state (first name)(chapter 5:use indexOf function)(to locate specific character in string)
+        'eg:     Emma, you are 7300 years old
+
+        'IndexOf explanation
+        'eg:     James Bond
+        'txtname.IndexOf(" ")
+        'eg: 23.45
+        'txtnum.IndexOf(".")
+
+        '8) subString explanation
+        '-->to split words into desired
+        'Dim a = James Bond
+        'a.subString(0, n) = James
+        'a.subString(0, n + 1) = Bond
+
+        'Dim i As Integer
+        ' Dim InpName As String
+
+        'For i = 1 To 4
+        ' InpName = InputBox("What is your name?")
+        '  If InpName = "computer literacy" Then
+        '  TextBox1.Text = InpName
+        '   ElseIf InpName = "engineering" Then
+        '  TextBox1.Text = InpName
+        'Else
+        '  MsgBox("We only offer computer literacy and engineering")
+        '    End If
+        '  Next i
+
+        Dim a As String
+        Dim b As Integer
+
+        a = InputBox("What is your name?")
+
+        Label1.Text = a
+
+        b = MsgBox("Hello " & a & ", please choose your birthdate")
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        Dim datecheck As String
+        Dim today As System.DateTime = System.DateTime.Now
+        datecheck = DateTimePicker1.Text
+
+        Dim yearnow As Integer = today.Year
+        Dim yeardtp As Integer = DateTimePicker1.Value.Year
+        Dim rightyear As Integer = DateTimePicker1.Value.Year
+        Dim minimage As Integer = yeardtp - rightyear
+        Dim currentage As Integer = yearnow - yeardtp
+
+        Label2.Text = currentage.ToString
+        Label2.Show()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim today As String = Date.Today
+        Dim dob As String = DateTimePicker1.Text
+
+        Dim d1 As DateTime = DateTime.Parse(today).Date
+        Dim d2 As DateTime = DateTime.Parse(dob).Date
+
+        Dim days As Single = (d1 - d2).TotalDays
+
+        'Dim fullName As String = Label1.Text
+        'Dim splitName As String() = fullName.Split(" ")
+        'Dim firstName As String = splitName(0).Substring(0, 1) & splitName(1).Substring(0, 1)
+
+        Label3.Text = Label1.Text.Split(" "c)(0) & ", you are " & days & " days old!"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Close()
+    End Sub
+End Class
